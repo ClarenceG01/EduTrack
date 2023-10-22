f0344a0d74a65f0629e5945383b77ee0f4e98bc6
ClarenceG01 <gatama001@gmail.com>
2023-10-22 11:39:32 +0300

    database querries

---
+++ Auth db queries/create tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Student(
+    student_id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+    registration_no VARCHAR(200) UNIQUE NOT NULL,
+    profile_pic VARCHAR(MAX),
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    year_of_study VARCHAR(200) NOT NULL,
+);
+CREATE TABLE Users(
+    users_id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+    email VARCHAR(200) UNIQUE NOT NULL,
+    pwd VARCHAR(600) NOT NULL,
+    registration_no VARCHAR(200) FOREIGN KEY REFERENCES Student(registration_no),
+    phone_number VARCHAR(200) NOT NULL,
+    created_at DATE DEFAULT GETDATE(),
+);
+CREATE TABLE Admin(
+    admin_id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+    username VARCHAR(200) UNIQUE NOT NULL,
+    pwd VARCHAR(600) NOT NULL,
+    phone_number VARCHAR(200) NOT NULL,
+    created_at DATE DEFAULT GETDATE(),
+);
+
---
+++ Auth db queries/dirt.sql
@@ -0,0 +1,13 @@
+--DIRT
+
+-- DROP ALL TABLES
+-- Path: drop tables.sql
+DROP TABLE Student;
+DROP TABLE Users;
+DROP TABLE Admin;
+
+--SELECT ALL TABLES
+-- Path: select tables.sql
+SELECT * FROM Student;
+SELECT * FROM Users;
+SELECT * FROM Admin;
