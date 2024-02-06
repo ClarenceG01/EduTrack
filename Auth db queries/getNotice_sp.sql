14dc2147d637a26f7434df0e45437afb94739daa
ClarenceG01 <gatama001@gmail.com>
2024-02-06 09:25:51 +0300

    changes to announcement module

--- Auth db queries/getNotice_sp.sql
+++ Auth db queries/getNotice_sp.sql
@@ -1,8 +1,6 @@
 CREATE OR ALTER PROCEDURE getNotice
 AS
 BEGIN
-    --fetch rows that are not over 14 days old
-    SELECT notice_title,file_path,notice_body,created_at,file_type,file_name
-    FROM Notice
-    WHERE DATEDIFF(day, created_at, GETDATE()) <= 14
+    --fetch rows that are not over 14 days old and sort them in order of most recent
+    SELECT TOP 10 * FROM Notice WHERE DATEDIFF(day, created_at, GETDATE()) <= 14 ORDER BY created_at DESC;
 END;
